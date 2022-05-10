2aa4876d81068b57fe3fe7942c70493a953c72b4
倖己 古川 <kkfrkw20140320sw123og@gmail.com>
2022-05-10 12:03:43 +0900

    消えていたやつを戻した

--- Project1/GameMainManager.cpp
+++ Project1/GameMainManager.cpp
@@ -21,6 +21,8 @@ void GameMainManager::Init(Stage* stageptr, Player* playerptr)
 
 	menuBGM = Audio::LoadSound_wav("Resources/sound/BGM/bgm01.wav");
 	playBGM = Audio::LoadSound_wav("Resource/sound/BGM/bgm02.wav");
+
+	ui.Init();
 }
 
 void GameMainManager::Update()
@@ -43,8 +45,16 @@ void GameMainManager::Finalize()
 
 void GameMainManager::GameInstanceUpdate()
 {
+	playerTile[0] = player->playerTile[0];
+	playerTile[1] = player->playerTile[1];
+	playerTile[2] = player->playerTile[2];
+	playerTile[3] = player->playerTile[3];
+
+	ui.Update(stage, player, playerTile, &Ischangecount);
+
+
 	//各ステージの処理
-//#ifdef _DEBUG
+#ifdef _DEBUG
 	if (Input::isKeyTrigger(DIK_1))
 	{
 		stage->LoadStage("./Resources/stage/stage1.csv", playerTile);
@@ -57,39 +67,37 @@ void GameMainManager::GameInstanceUpdate()
 		player->Init();
 		player->BodySetUp(playerTile);
 	}
-#ifdef _DEBUG
 	if (Input::isKeyTrigger(DIK_3))
 	{
 		stage->LoadStage("./Resources/stage/stage3.csv", playerTile);
 		player->Init();
 		player->BodySetUp(playerTile);
 	}
-
-#endif // _DEBUG
-
 	if (InputManger::ResetTrigger())
 	{
 		stage->Reset(playerTile);
 		player->Init();
 		player->BodySetUp(playerTile);
 	}
+#endif // _DEBUG
+
 	player->Update(*stage, drawOffsetX, drawOffsetY);
 	bool PlayerBodyStatus[4] = {};
 
 	player->SetBodyStatus(PlayerBodyStatus);
 
 	bool IsFolds[4] = {
-		player->IsLeftFold,
 		player->IsUpFold,
-		player->IsRightFold,
-		player->IsDownFold
+		player->IsDownFold,
+		player->IsLeftFold,
+		player->IsRightFold
 	};
 
 	bool IsOpens[4] = {
-		player->IsLeftOpen,
 		player->IsUpOpen,
-		player->IsRightOpen,
-		player->IsDownOpen
+		player->IsDownOpen,
+		player->IsLeftOpen,
+		player->IsRightOpen
 	};
 
 	stage->Updata();
@@ -117,6 +125,22 @@ void GameMainManager::GameInstanceUpdate()
 		player->OpenCount = 0;
 		player->IsOpenCountStart = false;
 	}
+
+	if (player->IsGoal && !Ischangecount)
+	{
+		Ischangecount = true;
+		changecount = 0;
+	}
+
+	if (Ischangecount)
+	{
+		changecount++;
+
+		if (changecount > 20)
+		{
+			IsGoSelect = true;
+		}
+	}
 }
 
 void GameMainManager::GameInstanceDraw()
@@ -126,6 +150,7 @@ void GameMainManager::GameInstanceDraw()
 	Back.DrawExtendSprite(0, 0, 1280, 720);
 	Back.Draw();
 	Raki_DX12B::Get()->ClearDepthBuffer();
+	ui.Draw();
 	stage->Draw(drawOffsetX, drawOffsetY);
 	player->Draw(drawOffsetX, drawOffsetY);
 }
