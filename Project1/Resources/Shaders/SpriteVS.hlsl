01ca1b6aa5ab8b817a6cd7c47d9d05ec53a58a6b
YusukeNara <rakyyongaming@gmail.com>
2022-05-15 15:12:35 +0900

    スプライトに色変更機能追加 Sprite::SetSpriteColorParam(float r, float g ,float b, float a);

--- Project1/Resources/Shaders/SpriteVS.hlsl
+++ Project1/Resources/Shaders/SpriteVS.hlsl
@@ -1,7 +1,11 @@
 #include "Sprite.hlsli"
 
 //スプライトの入力は1頂点のみ
-VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD,matrix instanceMatrix : INSTANCE_WORLD_MAT,float2 size : INSTANCE_DRAWSIZE,float4 uvOffset : INSTANCE_UVOFFSET)
+VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD,
+matrix instanceMatrix : INSTANCE_WORLD_MAT,
+float2 size : INSTANCE_DRAWSIZE,
+float4 uvOffset : INSTANCE_UVOFFSET,
+float4 color : INSTANCE_COLOR)
 {
 	VSOutput output;
     output.svpos = pos;
@@ -9,5 +13,6 @@ VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD,matrix instanceMatrix 
     output.ins_matrix = instanceMatrix;
     output.size = size;
     output.uvOffset = uvOffset;
+    output.color = color;
 	return output;
 }
