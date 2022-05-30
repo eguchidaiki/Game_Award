3b3fe697ac4b24976307731f9aa5f431dea7815f
エグチダイキ <daiki20011222@gmail.com>
2022-05-31 02:55:09 +0900

    ステージ選択のSEの鳴り方を少し修正

--- Project1/InputManger.h
+++ Project1/InputManger.h
@@ -47,85 +47,85 @@ public: //メンバ関数
 	// 終了(押した瞬間の判定)
 	bool EscapeTrigger();
 	// 終了(離した瞬間の判定)
-	bool EscapeReturn();
+	bool EscapeRelease();
 	// 上
 	bool Up();
 	// 上(押した瞬間の判定)
 	bool UpTrigger();
 	// 上(離した瞬間の判定)
-	bool UpReturn();
+	bool UpRelease();
 	// 下
 	bool Down();
 	// 下(押した瞬間の判定)
 	bool DownTrigger();
 	// 下(離した瞬間の判定)
-	bool DownReturn();
+	bool DownRelease();
 	// 左
 	bool Left();
 	// 左(押した瞬間の判定)
 	bool LeftTrigger();
 	// 左(離した瞬間の判定)
-	bool LeftReturn();
+	bool LeftRelease();
 	// 右
 	bool Right();
 	// 右(押した瞬間の判定)
 	bool RightTrigger();
 	// 右(離した瞬間の判定)
-	bool RightReturn();
+	bool RightRelease();
 	// 上(サブ)
 	bool SubUp();
 	// 上(サブ)(押した瞬間の判定)
 	bool SubUpTrigger();
 	// 上(サブ)(離した瞬間の判定)
-	bool SubUpReturn();
+	bool SubUpRelease();
 	// 下(サブ)
 	bool SubDown();
 	// 下(サブ)(押した瞬間の判定)
 	bool SubDownTrigger();
 	// 下(サブ)(離した瞬間の判定)
-	bool SubDownReturn();
+	bool SubDownRelease();
 	// 左(サブ)
 	bool SubLeft();
 	// 左(サブ)(押した瞬間の判定)
 	bool SubLeftTrigger();
 	// 左(サブ)(離した瞬間の判定)
-	bool SubLeftReturn();
+	bool SubLeftRelease();
 	// 右(サブ)
 	bool SubRight();
 	// 右(サブ)(押した瞬間の判定)
 	bool SubRightTrigger();
 	// 右(サブ)(離した瞬間の判定)
-	bool SubRightReturn();
+	bool SubRightRelease();
 	// 決定
 	bool Decision();
 	// 決定(押した瞬間の判定)
 	bool DecisionTrigger();
 	// 決定(離した瞬間の判定)
-	bool DecisionReturn();
+	bool DecisionRelease();
 	// キャンセル
 	bool Cancel();
 	// キャンセル(押した瞬間の判定)
 	bool CancelTrigger();
 	// キャンセル(離した瞬間の判定)
-	bool CancelReturn();
+	bool CancelRelease();
 	// リセット
 	bool Reset();
 	// リセット(押した瞬間の判定)
 	bool ResetTrigger();
 	// リセット(離した瞬間の判定)
-	bool ResetReturn();
+	bool ResetRelease();
 	// メニュー
 	bool Menu();
 	// メニュー(押した瞬間の判定)
 	bool MenuTrigger();
 	// メニュー(離した瞬間の判定)
-	bool MenuReturn();
+	bool MenuRelease();
 	// ジャンプ
 	bool Jump();
 	// ジャンプ(押した瞬間の判定)
 	bool JumpTrigger();
 	// ジャンプ(離した瞬間の判定)
-	bool JumpReturn();
+	bool JumpRelease();
 
 	//折る操作のトリガー
 	bool FoldLeftTrigger();
@@ -133,11 +133,23 @@ public: //メンバ関数
 	bool FoldRightTrigger();
 	bool FoldDownTrigger();
 
+	//折る操作のリリース
+	bool FoldLeftRelease();
+	bool FoldUpRelease();
+	bool FoldRightRelease();
+	bool FoldDownRelease();
+
 	//開く操作のトリガー
 	bool OpenLeftTrigger();
 	bool OpenUpTrigger();
 	bool OpenRightTrigger();
 	bool OpenDownTrigger();
+
+	//開く操作のリリース
+	bool OpenLeftRelease();
+	bool OpenUpRelease();
+	bool OpenRightRelease();
+	bool OpenDownRelease();
 
 	//折る開くのセレクト
 	bool FoldSelectTrigger();
